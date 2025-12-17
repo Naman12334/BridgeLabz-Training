9564d940ca15f4813978471d1c61693c6e5772e6
namanagrawalcs22 <141643219+namanagrawalcs22@users.noreply.github.com>
2025-12-17 16:16:15 +0530

    class_questions

---
+++ core-java-practice/gcr-codebase/calculate_simple_interest.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class calculate_simple_interest {
+     public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int p =sc.nextInt();
+        int r = sc.nextInt();
+        int t = sc.nextInt();
+        int ans = (p*r*t) / 100;
+        System.out.println(ans);
+     }
+
+    
+}
---
+++ core-java-practice/gcr-codebase/volume_of_cylinder.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class volume_of_cylinder {
+      public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int r = sc.nextInt();
+        int h = sc.nextInt();
+        int v = 22/7 * (r*r) * h;
+      }
+}
