532f0f6489943aee551c7890cb9c0a2b4cd1b3ac
namanagrawalcs22 <141643219+namanagrawalcs22@users.noreply.github.com>
2025-12-17 15:45:38 +0530

    class-questions

---
+++ core-java-practice/gcr-codebase/area_of_circle.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class area_of_circle {
+      public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int r = sc.nextInt();
+        int area = 22/7 * r * r;
+        System.out.println(area);
+
+
+
+      }
+}
