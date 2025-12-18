e46835a04b8627b07cd3d237f09a85467663cec5
namanagrawalcs22 <141643219+namanagrawalcs22@users.noreply.github.com>
2025-12-18 22:24:28 +0530

    Create ChalkBoardGame.java

---
+++ core-java-practice/leet-code-codebase/ChalkBoardGame.java
@@ -0,0 +1,21 @@
+
+public class ChalkBoardGame {
+    public boolean xorGame(int[] nums) {
+        int xor  =0;
+        for(int i=0;i<nums.length;i++){
+            xor = xor ^ nums[i];
+        }
+
+        if(xor == 0){
+            return true;
+        }
+        if(nums.length % 2 == 0){
+            return true;
+        }
+        return false;
+
+    }
+}
+
+    
+
