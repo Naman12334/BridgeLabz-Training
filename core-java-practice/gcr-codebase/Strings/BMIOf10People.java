a980ee8838535695e59fa0027a3b672c1a35b2aa
namanagrawalcs22 <141643219+namanagrawalcs22@users.noreply.github.com>
2025-12-22 22:16:40 +0530

    Naman Add. String questions completed

---
+++ core-java-practice/gcr-codebase/Strings/BMIOf10People.java
@@ -0,0 +1,4 @@
+
+public class BMIOf10People {
+
+}
---
+++ core-java-practice/gcr-codebase/Strings/ConvertToUpperCase.java
@@ -0,0 +1,9 @@
+
+public class ConvertToUpperCase {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
