36cc6ac6d854313b64aab29a58c017ceff790127
namanagrawalcs22 <141643219+namanagrawalcs22@users.noreply.github.com>
2026-01-27 10:42:51 +0530

    [Naman] Add . java streams question

---
+++ collections-practice/gcr-codebase/Collections/InvertMap.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+class InvertMap {
+    public static void main(String[] args) {
+        Map<String,Integer> map = Map.of("A",1,"B",2,"C",1);
+        Map<Integer,List<String>> result = new HashMap<>();
+
+        for(var e : map.entrySet())
+            result.computeIfAbsent(e.getValue(), k -> new ArrayList<>())
+                  .add(e.getKey());
+
+        System.out.println(result);
+    }
+}
