5bb5a8acdf8c8bb690ec345421f0bb86e29a2987
namanagrawalcs22 <141643219+namanagrawalcs22@users.noreply.github.com>
2026-02-09 12:52:13 +0530

    [Naman] Add. Implement program for sonarqube in address book

---
+++ oops-practice/gcr-codebase/addressbook/src/main/java/org/naman/sonarqube/App.java
@@ -0,0 +1,10 @@
+package org.naman.sonarqube;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ oops-practice/gcr-codebase/addressbook/src/main/java/org/naman/sonarqube/Calculator.java
@@ -0,0 +1,7 @@
+package org.naman.sonarqube;
+
+public class Calculator {
+	public int add(int a,int b) {
+		return a+b;
+	}
+}
